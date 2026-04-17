74d93bbbf45e4f513cb1fbb4c21f4668cfadf103
pateh27 <fathzfl@gmail.com>
2026-04-17 23:43:56 +0700

    Tugas Praktikum 5 - Mengubah avatar agar menggunakan next/image guna optimasi gambar

--- praktikum10/next.config.js
+++ praktikum10/next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    removeConsole: false,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatar.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
