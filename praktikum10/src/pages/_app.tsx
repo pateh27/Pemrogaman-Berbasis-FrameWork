fa8f0ed2b989c84c5e4868bf102b04c96c1be83b
pateh27 <fathzfl@gmail.com>
2026-04-22 14:48:00 +0700

    🔥 Tugas Praktikum 2-5 Jobsheet 18 done

--- praktikum10/src/pages/_app.tsx
+++ praktikum10/src/pages/_app.tsx
@@ -3,13 +3,30 @@ import type { AppProps } from 'next/app'
 import Navbar from '../components/layouts/navbar';
 import AppShell from '../components/layouts/Appshell';
 import { SessionProvider } from 'next-auth/react';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return( 
-    <SessionProvider session={pageProps.session}>
-      <AppShell>
-        <Component {...pageProps} />
-      </AppShell>
-    </SessionProvider>
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-LZT57MSHQQ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LZT57MSHQQ');
+        `}
+      </Script>
+
+      <SessionProvider session={pageProps.session}>
+        <AppShell>
+          <Component {...pageProps} />
+        </AppShell>
+      </SessionProvider>
+    </>
   );
-};
+}
